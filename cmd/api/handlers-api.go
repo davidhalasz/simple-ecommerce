8ca64b21fa34b5c787dadfc5b793d89d131def9c
davidhalasz <davidhalasz22@gmail.com>
2024-05-27 22:00:48 +0200

    checking athenticate has been implemented

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"gostripeapp/internal/cards"
 	"gostripeapp/internal/models"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -290,4 +292,50 @@ func (app *application) CreateAuthToken(w http.ResponseWriter, r *http.Request) 
 	payload.Token = token
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *application) authenticateToken(r *http.Request) (*models.User, error) {
+	authorizationHeader := r.Header.Get("Authorization")
+	app.infoLog.Println("Called 1")
+	if authorizationHeader == "" {
+		app.infoLog.Println("error 1")
+		return nil, errors.New("no authorization header received")
+	}
+
+	app.infoLog.Println("Called 2")
+	headerParts := strings.Split(authorizationHeader, " ")
+	if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+		app.infoLog.Println("error 2")
+		return nil, errors.New("no authorization header received")
+	}
+
+	token := headerParts[1]
+	if len(token) != 26 {
+		app.infoLog.Println("error 3")
+		return nil, errors.New("authentication token wrong size")
+	}
+
+	user, err := app.DB.GetUserForToken(token)
+	if err != nil {
+		return nil, errors.New("no matching user found")
+	}
+
+	return user, nil
+}
+
+func (app *application) CheckAuthentication(w http.ResponseWriter, r *http.Request) {
+	// validate the token and get associated user
+	user, err := app.authenticateToken(r)
+	if err != nil {
+		app.invalidCredentials(w)
+		return
+	}
+
+	var payload struct {
+		Error   bool   `json:"error"`
+		Message string `json:"message"`
+	}
+	payload.Error = false
+	payload.Message = fmt.Sprintf("authenticated user %s", user.Email)
+	app.invalidCredentials(w)
 }
